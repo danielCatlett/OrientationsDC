a090fe2da95b3ca57a477ca1305a109a1f4cc35d
danielCatlett <daniel.catlett42@gmail.com>
2017-02-20 17:59:20 -0600

    Fun with orientations
    RIP individual view restrictions

--- Orientations/ViewController.swift
+++ Orientations/ViewController.swift
@@ -1,25 +1,29 @@
-//
 //  ViewController.swift
 //  Orientations
 //
 //  Created by Daniel Catlett on 2/20/17.
 //  Copyright © 2017 Daniel Catlett. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    /* This doesn't exist anymore, kinda dumb
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask
+    {
+        return UIInterfaceOrientationMask(rawValue:
+            (UIInterfaceOrientationMask.portrait.rawValue | UIInterfaceOrientationMask.landscapeLeft.rawValue))
+    }
+    */
 
 }
-
